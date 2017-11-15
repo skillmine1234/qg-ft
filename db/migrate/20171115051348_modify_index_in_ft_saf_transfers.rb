ee088f5868db3bcccb6de26738ce002ad766a211
Neha1993 <neha.upadhyay@quantiguous.com>
2017-11-15 15:14:36 +0530

    SY-167: changed the index name

--- db/migrate/20171115051348_modify_index_in_ft_saf_transfers.rb
+++ db/migrate/20171115051348_modify_index_in_ft_saf_transfers.rb
@@ -2,7 +2,7 @@ class ModifyIndexInFtSafTransfers < ActiveRecord::Migration
   def change
     if Rails.configuration.database_configuration[Rails.env]["adapter"] == 'oracle_enhanced'
       execute 'drop index uk_ft_saf_transfers_02'
-      execute 'create index uk_ft_saf_transfers_02 on ft_saf_transfers (trunc(req_timestamp), req_transfer_type, customer_id)'
+      execute 'create index ft_saf_transfers_02 on ft_saf_transfers (trunc(req_timestamp), req_transfer_type, customer_id)'
     end    
   end
 end
