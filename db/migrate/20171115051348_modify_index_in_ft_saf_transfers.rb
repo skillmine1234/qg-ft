646caec6d1948198beea8f66e0ad2d6818033b75
divyajayan <divya.jayan@quantiguous.com>
2017-11-15 16:51:18 +0530

    SY-167 fixed index name

--- db/migrate/20171115051348_modify_index_in_ft_saf_transfers.rb
+++ db/migrate/20171115051348_modify_index_in_ft_saf_transfers.rb
@@ -1,7 +1,7 @@
 class ModifyIndexInFtSafTransfers < ActiveRecord::Migration
   def change
     if Rails.configuration.database_configuration[Rails.env]["adapter"] == 'oracle_enhanced'
-      execute 'drop index uk_ft_saf_transfers_02'
+      execute 'drop index ft_saf_transfers_02'
       execute 'create index ft_saf_transfers_02 on ft_saf_transfers (trunc(req_timestamp), req_transfer_type, customer_id)'
     end    
   end
