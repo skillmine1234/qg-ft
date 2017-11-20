891da0c759e2276030811c60f34b07e72b5141b6
divyajayan <divya.jayan@quantiguous.com>
2017-11-20 11:35:02 +0530

    SY-167 small fixes

--- db/migrate/20171118084422_change_columns_in_funds_transfers.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnsInFundsTransfers < ActiveRecord::Migration
-  def change
-    if ActiveRecord::Base.connection.table_exists? 'funds_transfers'
-      change_column :funds_transfers, :was_saf, :string, null: true, default: nil
-      change_column :funds_transfers, :op_name, :string, null: true, default: nil
-    end
-  end
-end
