e19850ee72f6094437d2df920fa32e5cd095c010
QG <qg@QGs-MacBook-Pro-4.local>
2018-02-20 10:32:53 +0530

    sy-173 added default value as N in allowed_upi column

--- db/migrate/20180122140630_add_column_allow_upi_in_ft_customers.rb
+++ db/migrate/20180122140630_add_column_allow_upi_in_ft_customers.rb
@@ -1,5 +1,5 @@
 class AddColumnAllowUpiInFtCustomers < ActiveRecord::Migration
   def change
-    add_column :ft_customers, :allow_upi, :string, :limit => 1, comment: 'the identifier to specify if upi transaction is allowed or not to the customer' 
+    add_column :ft_customers, :allow_upi, :string, :limit => 1, null: false, default: 'N', comment: 'the identifier to specify if upi transaction is allowed or not to the customer' 
   end
 end
