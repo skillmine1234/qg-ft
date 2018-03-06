a3e36281ebf115cd35ad7774d00d372101897df4
divyajayan <divya.jayan@quantiguous.com>
2018-03-06 12:10:38 +0530

    small fixes

--- db/migrate/20180220132119_add_attempt_no_to_pending_funds_transfers.rb
+++ db/migrate/20180220132119_add_attempt_no_to_pending_funds_transfers.rb
@@ -1,6 +1,6 @@
 class AddAttemptNoToPendingFundsTransfers < ActiveRecord::Migration
   def change
-    if ActiveRecord::Base.connection.table_exists? 'funds_transfers'
+    if ActiveRecord::Base.connection.table_exists? 'pending_funds_transfers'
       add_column :pending_funds_transfers, :attempt_no, :integer, comment: 'the attempt number of the requery'
     end
   end
