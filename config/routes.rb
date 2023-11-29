1cc2993df17a881876dc709ee06828579dd9c154
skillmine1234 <rahul.khoragade@skill-mine.com>
2023-11-29 08:39:11 +0530

    ew changes patch methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :funds_transfers, :inward_remittances, :whitelisted_identities, only: [:index, :show] do
     collection do
-      put :index
+      post :index
     end
     member do
       get :steps
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
   resources :ft_unapproved_records
   resources :funds_transfer_customers do
     member do
-      put :resend_notification
+      post :resend_notification
     end
     collection do
-      put :index
+      post :index
       get :validate_customer_id_ft_customer
       get :validate_app_id_ft_customer
       post :create_clone_accounts
@@ -27,15 +27,17 @@ Rails.application.routes.draw do
       get :validate_app_id_customer_id_ft_customer
       match :deactivate_user, via: [:get, :post]
       post :deactivation_customer_final_list
+      patch :index
     end
   end
 
   resources :ft_customer_disable_lists
-  put '/ft_customer_disable_lists/:id/approve' => "ft_customer_disable_lists#approve"
+  post '/ft_customer_disable_lists/:id/approve' => "ft_customer_disable_lists#approve"
 
   resources :ft_customer_accounts, :ft_purpose_codes, :ft_incoming_records do
     collection do
-      put :index
+      post :index
+      patch :index
     end
   end
 
@@ -43,38 +45,43 @@ Rails.application.routes.draw do
     collection do
       get :index
       post :index
+      patch :index
     end
   end
   resources :ft_saf_transfers, except: :index do
     collection do
       get :index
-      put :index
+      post :index
+      patch :index
     end
     member do
-      put :delete
+      post :delete
     end    
   end
   
   resources :ft_purge_saf_transfers, except: [:edit, :update] do
     member do
-      put :approve
+      post :approve
+      patch :index
     end
   end
   
   resources :nach_members do
     collection do
       get :index
-      put :index
+      post :index
+      patch :index
     end
     member do
-      put :approve
+      post :approve
       get :audit_logs
     end
   end
   
   resources :ft_invoice_details do
     collection do
-      put :index
+      post :index
+      patch :index
     end
     member do
       get :steps
@@ -87,9 +94,9 @@ Rails.application.routes.draw do
   get '/ft_incoming_records/:id/audit_logs' => 'ft_incoming_records#audit_logs'
   get '/ft_purpose_code/:id/audit_logs' => 'ft_purpose_codes#audit_logs'
   get '/ft_customer_account/:id/audit_logs' => 'ft_customer_accounts#audit_logs'
-  put '/funds_transfer_customer/:id/approve' => "funds_transfer_customers#approve"
-  put '/ft_purpose_code/:id/approve' => "ft_purpose_codes#approve"
-  put '/ft_customer_account/:id/approve' => "ft_customer_accounts#approve"
+  post '/funds_transfer_customer/:id/approve' => "funds_transfer_customers#approve"
+  post '/ft_purpose_code/:id/approve' => "ft_purpose_codes#approve"
+  post '/ft_customer_account/:id/approve' => "ft_customer_accounts#approve"
   get 'ft_apbs_incoming_file_summary' => 'ft_apbs_incoming_records#incoming_file_summary'
   get '/ft_apbs_incoming_records/:id/audit_logs' => 'ft_apbs_incoming_records#audit_logs'
   get '/funds_transfer_customer/:id/audit_logs' => 'funds_transfer_customers#audit_logs'
